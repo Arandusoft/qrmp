0cbcd6cce9191abfe88268077edc053437243dd9
lainz <leandrodiazoe@gmail.com>
2019-04-17 11:33:09 -0300

    Damian: fix tamaño

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ var idPV = urlParams.get('idpv');
 if (document.getElementById("qrcode")) {
     qrcode = new QRCode(document.getElementById("qrcode"), {
         text: "",
-        width: 512,
-        height: 512,
+        width: 300,
+        height: 300,
         colorDark : "#000000",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
     });
     qrcode.clear();
     qrcode.makeCode('https://mercadopago.com/s/qr/' + mpUser_id + '/pv' + idPV);
+    console.log('https://mercadopago.com/s/qr/' + mpUser_id + '/pv' + idPV)
 
     document.getElementById('pv').innerHTML = 'Punto de Venta ' + idPV;
 }
